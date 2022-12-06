8dfb2eecf2d41b437368a119658092dfe7bbad09
Lukas <lukasz.holowacz5@gmail.com>
2022-12-06 21:04:25 +0100

    przenoszenie z main activity za pomocą przycisków na posczególne activity, modyfikacja styli oraz kolorystyki aplikacji oraz kilka lekkich zmian

--- app/src/main/java/com/example/zastosowaniesortowan/MainActivity.kt
+++ app/src/main/java/com/example/zastosowaniesortowan/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.example.zastosowaniesortowan
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val przyciski = listOf<Button>(
+            findViewById(R.id.bubble),
+            findViewById(R.id.fast),
+            findViewById(R.id.scalanie),
+            findViewById(R.id.wstawianie),
+            findViewById(R.id.heapsort)
+        )
+
+        przyciski[0].setOnClickListener {
+            val intent = Intent(this,BabelkoweActivity::class.java)
+            startActivity(intent)
+        }
+        przyciski[1].setOnClickListener {
+            val intent = Intent(this,SzybkieActivity::class.java)
+            startActivity(intent)
+        }
+        przyciski[2].setOnClickListener {
+            val intent = Intent(this,ScalanieActivity::class.java)
+            startActivity(intent)
+        }
+        przyciski[3].setOnClickListener {
+            val intent = Intent(this,WstawianieActivity::class.java)
+            startActivity(intent)
+        }
+        przyciski[4].setOnClickListener {
+            val intent = Intent(this,BabelkoweActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/zastosowaniesortowan/WstawianieActivity.kt
@@ -0,0 +1,11 @@
+package com.example.zastosowaniesortowan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WstawianieActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_wstawianie)
+    }
+}
